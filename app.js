84432228e898e2143f5b190f0012fb1adfd46244
VirAlex <alexou73@gmail.com>
2020-01-17 18:35:21 +0100

    add fonctions delete

--- app.js
+++ app.js
@@ -10,11 +10,23 @@ buttonresult.addEventListener('click', () => {
     const api = `${proxy}http://data.nba.net/10s/prod/v1/today.json`;
     fetchGameScore();
   document.querySelector('#butres').innerHTML='';
+  document.querySelector('#butres').innerHTML = '<a href="#" id="buttonveille"><span>Veille</span></a>';
+  let buttonVeille = document.querySelector("#buttonveille")
+  buttonVeille.addEventListener('click', () => {
+    console.log("Hello");
+    document.querySelector('#result').innerHTML = '';
+    fetchPrevMatchs();
+  })
 })
 
 buttonother.addEventListener('click', () => {
   document.querySelector('#result').innerHTML = '';
   document.querySelector('#butres').innerHTML = '<a href="#" id="buttonresult"><span>Resultats</span></a>'
+  let buttonresult = document.querySelector('#butres');
+  buttonresult.addEventListener('click', () => {
+    fetchGameScore();
+    ;
+  })
 })
 
 buttontommorow.addEventListener('click', () => {
@@ -113,4 +125,55 @@ function fetchNextMatchs() {
       output += '</ul>'
       vTeam.innerHTML = output;
   })
+}
+
+function fetchPrevMatchs() {
+  let vTeam = document.getElementById('result');
+  let result = document.querySelector("#result");
+  const proxy = `https://cors-anywhere.herokuapp.com/`;
+  var today = new Date();
+  var d = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = today.getFullYear();
+
+  today = yyyy + mm + (d-1);
+  console.log(today);
+
+  const api = `${proxy}http://data.nba.net/10s/prod/v2/${today}/scoreboard.json`;
+  fetch(api)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      let numGames = data.numGames
+      let output = `<div class="titre">
+      <h2>Tonight ${numGames} matchs</h2>
+      </div>`;
+      const games = data.games;
+      games.forEach(game => {
+        console.log(game);
+        output += ` <div class="vTeam">
+                      <div class="series">
+                      <h2 class="vName">${game.vTeam.triCode}</h2>
+                      <p> W ${game.vTeam.win} L ${game.vTeam.loss} </p>
+                      <p> actuellement série de ${game.vTeam.seriesWin}  </p>
+                      </div>
+                      <p class="score">${game.vTeam.score}</p>
+                      <h2>VS</h2>
+                      <p class="score">${game.hTeam.score}</p>
+                      <div class="series">
+                      <h2 class="dTeam">${game.hTeam.triCode}</h2>
+                      <p> W ${game.hTeam.win} L ${game.hTeam.loss} </p>
+                      <p> actuellement série de ${game.hTeam.seriesWin}  </p>
+                      </div>
+                    </div>
+                    <div class="mark">
+                      <h4>${game.nugget.text}</h4>
+                    </div>
+                        `;
+      });
+      output += '</ul>'
+      vTeam.innerHTML = output;
+    })
 }

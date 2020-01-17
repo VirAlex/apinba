b4f21f544a0731b304265dc242b5442552fcfd20
VirAlex <alexou73@gmail.com>
2020-01-17 16:36:53 +0100

    style

--- app.js
+++ app.js
@@ -1,54 +1,55 @@
-window.addEventListener('load', () => {
+// window.addEventListener('load', () => {
 
-    console.log("Hello");
-    let vTeam = document.querySelector('.vTeam');
-    let dTeam = document.querySelector('.dTeam');
-
-
-
-
-    const proxy = `https://cors-anywhere.herokuapp.com/`;
-    const api = `${proxy}http://data.nba.net/10s/prod/v1/today.json`;
-    fetchGameScore();
-})
+//     console.log("Hello");
+//     let vTeam = document.querySelector('.vTeam');
+//     let dTeam = document.querySelector('.dTeam');
 
 
 
-function fetchGameScore() {
-    let vTeam = document.getElementById('result');
-    let dTeam = document.querySelector('.dTeam');
-    const proxy = `https://cors-anywhere.herokuapp.com/`;
-    const api = `${proxy}http://data.nba.net/10s/prod/v1/today.json`;
-    fetch(api)
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            const today = data.links.todayScoreboard;
-            fetch(`${proxy}http://data.nba.net/10s/${today}`)
-                .then(response => {
-                    return response.json();
-                })
-                .then(data => {
-                    let output = '';
-                    // output += '<ul>';
-                    const games = data.games;
-                    games.forEach(game => {
-                        console.log(game);
-                        output += ` <div class="vTeam">
-                        <h2 class="vName">${game.vTeam.triCode}</h2>
-                        <p>${game.vTeam.score}</p>
-                        <h2>VS</h2>
-                        <p>${game.hTeam.score}</p>
-                        <h2 class="dTeam">${game.hTeam.triCode}</h2>
-                        </div>
-                        `;
-                        // const vTeamFind = game.vTeam.triCode;
-                        // vTeam.textContent = vTeamFind
-                        // dTeam.textContent = game.hTeam.triCode;
-                    });
-                    output += '</ul>'
-                    vTeam.innerHTML = output;
-                })
-        })
-}
+
+//     const proxy = `https://cors-anywhere.herokuapp.com/`;
+//     const api = `${proxy}http://data.nba.net/10s/prod/v1/today.json`;
+//     fetchGameScore();
+// })
+
+
+
+// function fetchGameScore() {
+//     let vTeam = document.getElementById('result');
+//     let dTeam = document.querySelector('.dTeam');
+//     const proxy = `https://cors-anywhere.herokuapp.com/`;
+//     const api = `${proxy}http://data.nba.net/10s/prod/v1/today.json`;
+//     fetch(api)
+//         .then(response => {
+//             return response.json();
+//         })
+//         .then(data => {
+//             const today = data.links.todayScoreboard;
+//             fetch(`${proxy}http://data.nba.net/10s/${today}`)
+//                 .then(response => {
+//                     return response.json();
+//                 })
+//                 .then(data => {
+//                     let output = '';
+//                     // output += '<ul>';
+//                     const games = data.games;
+//                     games.forEach(game => {
+//                         console.log(game);
+//                         output += ` <div class="vTeam">
+//                         <h2 class="vName">${game.vTeam.triCode}</h2>
+//                         <p>${game.vTeam.score}</p>
+//                         <h2>VS</h2>
+//                         <p>${game.hTeam.score}</p>
+//                         <h2 class="dTeam">${game.hTeam.triCode}</h2>
+//                         </div>
+//                         <h4>${game.nugget.text}</h4>
+//                         `;
+//                         // const vTeamFind = game.vTeam.triCode;
+//                         // vTeam.textContent = vTeamFind
+//                         // dTeam.textContent = game.hTeam.triCode;
+//                     });
+//                     output += '</ul>'
+//                     vTeam.innerHTML = output;
+//                 })
+//         })
+// }
